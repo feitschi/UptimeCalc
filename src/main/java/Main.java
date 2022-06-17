d6c67afa7f789bc0767694d3286416b5274a6a9b
AndreasFeiertag <andreasfeiertag@outlook.at>
2022-06-17 20:40:44 +0200

    First test (unit test and browser test works)

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.concurrent.Executor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting REST Server...");
+        ResourceConfig rc = new ResourceConfig().packages("at.feiertag");
+        HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), rc);
+
+        System.out.println("Server started.");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.stop(0);
+        System.out.println("Server stopped.");
+
+    }
+}
---
+++ src/main/java/at/feiertag/UptimeController.java
@@ -0,0 +1,24 @@
+package at.feiertag;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+
+@Path("/api/uptime")
+public class UptimeController {
+
+    @GET
+    @Path("/{uptime}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateUptime(@PathParam("uptime") double uptime){
+        double minutesOfMonth = 60*24*30;
+        double definedUptimeInPercent = uptime/100;
+        double uptimeInMinutes = minutesOfMonth * definedUptimeInPercent;
+        String result = String.format("%.2f", uptimeInMinutes);
+        return result;
+    }
+}
